920dc4f27930e0ae22437fa382f99e9c4f0f0ca3
axel-zielonka <azielonka@fi.uba.ar>
2025-09-27 10:56:55 -0300

    added config

--- src/controllers/sorts/sort_by_count/main.py
+++ src/controllers/sorts/sort_by_count/main.py
@@ -1,6 +1,9 @@
-from sort_by_count import SortByCount
+from sort import SortByCount
+from shared import initializer
 
 def main():
+    config_params = initializer.init_config(["LOGGING_LEVEL"])
+    initializer.init_log(config_params["LOGGING_LEVEL"])
     sorter = SortByCount()
     sorter.start()
 
--- src/controllers/sorts/sort_by_sum/main.py
+++ src/controllers/sorts/sort_by_sum/main.py
@@ -1,6 +1,9 @@
-from sort_by_sum import SortBySum
+from sort import SortBySum
+from shared import initializer
 
 def main():
+    config_params = initializer.init_config(["LOGGING_LEVEL"])
+    initializer.init_log(config_params["LOGGING_LEVEL"])
     sorter = SortBySum()
     sorter.start()
 

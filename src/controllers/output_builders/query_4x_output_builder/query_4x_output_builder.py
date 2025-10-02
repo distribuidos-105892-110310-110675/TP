1c8e786c9e32bfd835aa087fbacb82f9d4c8de01
Luciano Martín Gamberale <lgamberale@fi.uba.ar>
2025-10-02 05:03:35 +0000

    :art: remove extra data

--- src/controllers/output_builders/query_4x_output_builder/query_4x_output_builder.py
+++ src/controllers/output_builders/query_4x_output_builder/query_4x_output_builder.py
@@ -9,7 +9,7 @@ class Query4XOutputBuilder(QueryOutputBuilder):
     # ============================== PRIVATE - INTERFACE ============================== #
 
     def columns_to_keep(self) -> list[str]:
-        return ["store_name", "birthdate"]
+        return ["store_name", "birthdate", "purchases_qty"]
 
     def output_message_type(self) -> str:
         return communication_protocol.QUERY_RESULT_4X_MSG_TYPE
